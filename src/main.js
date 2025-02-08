ed29092725350e2b8a5a91ea2e82687ded19c524
PushEax <pusheax.vk@gmail.com>
2025-02-08 19:51:12 +0200

    update

--- src/main.js
+++ src/main.js
@@ -3,22 +3,6 @@ import { refs } from './js/refs';
 import { createMarkup, createCard } from './js/markup-tasks';
 import { renderHTML } from './js/render-tasks';
 import { getStorageData, writeStorageData } from './js/local-storage-api';
-/*
-  Створи список справ.
-  На сторінці є два інпути які має вводиться назва і текст задачі.
-  Після натискання на кнопку "Add" завдання додається до списку #task-list.
-
-  У кожної картки має бути кнопка "Delete", щоб можна було
-  прибрати завдання зі списку.
-  Список із завданнями має бути доступним після перезавантаження сторінки.
-
-  Розмітка картки задачі
-  <li class="task-list-item">
-      <button class="task-list-item-btn">Delete</button>
-      <h3>Заголовок</h3>
-      <p>Текст</p>
-  </li>
-*/
 
 let cards = getStorageData();
 
@@ -31,11 +15,12 @@ function onFormSubmit(event) {
 
   const name = refs.form.elements.taskName.value.trim();
   const descr = refs.form.elements.taskDescription.value.trim();
+  const cardId = nanoid();
 
   const card = {
     name,
     description: descr,
-    id: nanoid(),
+    id: cardId,
   };
 
   cards.push(card);
